7a3024b3073640ec507bd962eb5d6788e5629105
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2020-09-17 16:06:17 +0100

    app component initial code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
   <div id="app">
+    <div>
+      {{data}}
+    </div>
+    <SearchBar />
+    <InfoBoard></InfoBoard>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SearchBar from "./components/SearchBar";
+import InfoBoard from "./components/InfoBoard";
+const apiKey = process.env.VUE_APP_API;
+const url = `https://geo.ipify.org/api/v1?apiKey=${apiKey}`;
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  } 
-}
+    SearchBar,
+    InfoBoard,
+  },
+  mounted() {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;1,700&display=swap");
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Rubik", sans-serif;
 }
 </style>
